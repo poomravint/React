ef507b6cee141c2e3ffcc0c338d3dc1fb466a64e
Ravint Wongwaro <poomravintwongwaro@gmail.com>
2022-08-10 15:12:59 +0700

    NewUpdate01

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
-const word = "Hello";
+
+//const word = "Hello";
+var words = ['Hello', 'Sawasdee']
+var word = words[Math.floor(Math.random()*words.length)];
+
+
 function App() {
 return (
-    <div>
-    <WordCard value="hello"/>
-    </div>
+        
+        <div>
+            <WordCard value={word}/>
+        </div>
+    
     );
 }
 export default App;
